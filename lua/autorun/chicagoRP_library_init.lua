e7613d43935e0ba130d0d43e5970f2c028afbc96
SpiffyJUNIOR <razer1546@gmail.com>
2023-05-06 19:52:23 -0700

    Dialog Box

--- lua/autorun/chicagoRP_library_init.lua
+++ lua/autorun/chicagoRP_library_init.lua
@@ -3,6 +3,7 @@ AddCSLuaFile()
 chicagoRP = chicagoRP or {}
 
 if CLIENT then
+    include("chicagorp_library/derma/cl_dialogbox.lua")
     include("chicagorp_library/drawing/cl_blur.lua")
     include("chicagorp_library/drawing/cl_fade.lua")
     include("chicagorp_library/drawing/cl_gradient.lua")
@@ -11,6 +12,7 @@ if CLIENT then
     include("chicagorp_library/drawing/cl_screenscale.lua")
     include("chicagorp_library/hooks/cl_hidehud.lua")
 else
+    AddCSLuaFile("chicagorp_library/derma/cl_dialogbox.lua")
     AddCSLuaFile("chicagorp_library/drawing/cl_blur.lua")
     AddCSLuaFile("chicagorp_library/drawing/cl_fade.lua")
     AddCSLuaFile("chicagorp_library/drawing/cl_gradient.lua")
@@ -20,5 +22,5 @@ else
     AddCSLuaFile("chicagorp_library/hooks/cl_hidehud.lua")
 end
 
-AddCSLuaFile("chicagorp_library/util/sh_uuid.lua")
 include("chicagorp_library/util/sh_uuid.lua")
+AddCSLuaFile("chicagorp_library/util/sh_uuid.lua")
